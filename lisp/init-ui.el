c8e6655b205c97abfe2e690eb26678de3bc808be
d2bot <d2botroot@gmail.com>
2022-04-14 13:56:49 +0800

    update

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -15,11 +15,37 @@
 (modus-themes-load-operandi)       ;; Light Theme
 ;; (modus-themes-load-vivendi)     ;; Dark Theme
 
-;; I'm activating mini-modeline after smart-mode-line
-(setq mini-modeline-face-attr 'default)
-(use-package mini-modeline)
-(mini-modeline-mode)
-(use-package mood-line)
+(use-package telephone-line
+  :ensure t
+  :init
+  (setq telephone-line-lhs
+                '((evil . (telephone-line-evil-tag-segment))
+                  (accent . (telephone-line-vc-segment
+                             telephone-line-erc-modified-channels-segment
+                             telephone-line-process-segment))
+                  (nil . (telephone-line-projectile-segment
+                          telephone-line-buffer-segment))))
+  (setq telephone-line-rhs
+                '((nil . (telephone-line-flycheck-segment
+                          telephone-line-misc-info-segment))
+                  (accent . (telephone-line-major-mode-segment))
+                  (evil (telephone-line-airline-position-segment))))
+  :custom-face
+  (telephone-line-evil-normal      ((t (:inherit telephone-line-evil :foreground "#d5c4a1" :background "#665c54"))))
+  (telephone-line-evil-insert      ((t (:inherit telephone-line-evil :foreground "#282828" :background "#83a598"))))
+  (telephone-line-evil-replace     ((t (:inherit telephone-line-evil :foreground "#282828" :background "#8ec07c"))))
+  (telephone-line-evil-visual      ((t (:inherit telephone-line-evil :foreground "#282828" :background "#fe8019"))))
+  (telephone-line-evil-operator    ((t (:inherit telephone-line-evil :foreground "#282828" :background "#fabd2f"))))
+  (telephone-line-evil-emacs       ((t (:inherit telephone-line-evil :foreground "#282828" :background "#d3869b"))))
+  (telephone-line-evil-motion      ((t (:inherit telephone-line-evil :foreground "#282828" :background "#b8bb26"))))
+  (telephone-line-accent-inactive  ((t (:inherit mode-line-inactive  :foreground "#ebdbb2" :background "#282828"))))
+  (telephone-line-accent-active    ((t (:inherit mode-line           :foreground "#ebdbb2" :background "#282828" :weight bold))))
+  (telephone-line-projectile       ((t (:inherit mode-line           :foreground "#83a598" :weight     bold))))
+  (telephone-line-unimportant      ((t (:inherit mode-line           :foreground "#7c6f64"))))
+  :config
+  (telephone-line-mode +1))
+  (require 'telephone-line)
+  (telephone-line-mode 1)
 
 ;;(use-package all-the-icons
 ;;  :if (display-graphic-p))
@@ -31,7 +57,6 @@
 (use-package evil)
 (require 'evil)
 (evil-mode 1)
-
 
 ;; treemacs
 (use-package treemacs
